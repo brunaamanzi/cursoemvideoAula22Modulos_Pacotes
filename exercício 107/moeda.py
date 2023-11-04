b04e370a03039a6a7ae94c6bc54153401f2dc9df
datasciente ADA <brunaamanzi@gmail.com>
2023-11-04 14:59:34 -0300

    Initial commit

---
+++ exercício 107/moeda.py
@@ -0,0 +1,15 @@
+def aumentar(n,p):
+    perc = p / 100
+    a = n * (1+perc)
+    return a
+
+def diminuir(n,p):
+    perc = p / 100
+    a = n * (1-perc)
+    return a
+
+def dobro(n):
+    return n * 2
+
+def metade(n):
+    return n / 2
---
+++ numeros.py
@@ -0,0 +1,7 @@
+# Aula 22: Módulos e Pacotes: ligando funções em arquivos diferentes
+from uteis import numeros
+
+num = int(input('Digite um valor: '))
+fat = numeros.fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} é {numeros.dobro(num)}')
